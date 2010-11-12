f5be1191843a78a63ab3f7f4ffa4391a86afcee7
Andre <azdr3milao@gmail.com>
2010-11-12 18:38:51 -0200

    modified:   app/controllers/profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,10 @@ class Profile < ActionController::Base
 def update_state_select
     states = Region.where(:country_id=>params[:id]).order(:name) unless params[:id].blank?
     render :partial => "states", :locals => { :states => states }
+
+end
+def update_city_select
+    cities = City.where(:region_id=>params[:id]).order(:name) unless params[:id].blank?
+    render :partial => "cities", :locals => { :cities => cities }
+end
 end
