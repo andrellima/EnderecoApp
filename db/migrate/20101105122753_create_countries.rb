85002d5fbaa440653ed5f37ba5ce352b2f2d29e6
Mozart Diniz <mozartdiniz@DARKNESS.local>
2010-11-11 22:41:57 -0300

    coloquei o end que faltava

--- db/migrate/20101105122753_create_countries.rb
+++ db/migrate/20101105122753_create_countries.rb
@@ -1,18 +1,23 @@
 class CreateCountries < ActiveRecord::Migration
-  def self.up
-    create_table :countries, :id => false do |t|
-    t.string    :code  ,:limit => 20
-    t.string    :code3 ,:limit => 30, :null=> true 
-    t.integer   :numcode , :null => true
-    t.string    :url 
-    t.string    :name
-    t.integer   :latitude
-    t.integer   :longitude
-    t.timestamp
-    execute "alter table countries modify column code varchar unsigned primary key"
   
+  def self.up
+    
+      create_table :countries, :id => false do |t|
+      t.string    :code  ,:limit => 20
+      t.string    :code3 ,:limit => 30, :null=> true 
+      t.integer   :numcode , :null => true
+      t.string    :url 
+      t.string    :name
+      t.integer   :latitude
+      t.integer   :longitude
+      t.timestamp
+      execute "alter table countries modify column code varchar unsigned primary key"
+  
+    end
+  
+    add_index(:countries, :name)
+
   end
-add_index(:countries, :name)
 
   def self.down
   end
